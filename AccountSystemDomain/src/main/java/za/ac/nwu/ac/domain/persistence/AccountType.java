2ddcc97bf4e88a11cd1599ec929f5064d3bf29ba
Andréas J. Otto <milkyotto@gmail.com>
2021-09-29 07:57:36 +0200

    start of DB implementation

--- AccountSystemDomain/src/main/java/za/ac/nwu/ac/domain/persistence/AccountType.java
+++ AccountSystemDomain/src/main/java/za/ac/nwu/ac/domain/persistence/AccountType.java
@@ -1,14 +1,24 @@
 package za.ac.nwu.ac.domain.persistence;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.io.Serializable;
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "ACCOUNT_TYPE", schema = "ANDREAS")
 public class AccountType implements Serializable {
     @Id
-    @SequenceGenerator(name)
+    @SequenceGenerator(name = "GENERIC_SEQ",sequenceName = "ANDREAS.GENERIC_SEQ",allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "GENERIC_SEQ")
+    @Column(name = "ACCOUNT_TYPE_ID")
+    private Long accountTypeId;
+
+    @Column(name = "MNEMONIC")
+    private String mnemonic;
+
+    @Column(name = "ACCOUNT_TYPE_NAME")
+    private String accountTypeName;
+
+    @Column(name = "CREATION_DATE")
+    private LocalDate createDate;
 }
